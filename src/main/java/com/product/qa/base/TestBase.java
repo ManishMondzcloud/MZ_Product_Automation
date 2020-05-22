9e7239465fabbf5e7cd7ff21392a0d4753d372b2
ManishMondzcloud <62377666+ManishMondzcloud@users.noreply.github.com>
2020-05-22 15:43:43 +0530

    b13
    b13

--- src/main/java/com/product/qa/base/TestBase.java
+++ src/main/java/com/product/qa/base/TestBase.java
@@ -91,7 +91,7 @@ public class TestBase
 			 */
 			
 			
-			System.setProperty("webdriver.chrome.driver", "driver//chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", "driver//chromedriver");
 			// Create object for ChromeDriver
 
 			driver = new ChromeDriver();
